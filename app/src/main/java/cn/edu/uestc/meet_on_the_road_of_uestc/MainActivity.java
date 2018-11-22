a71f119073565b8a5d8dd18bc3a6250eb1b379a3
ray <romerxzh@foxmail.com>
2018-11-22 15:07:15 +0800

    Init Meet_on_the_road_of_UESTC

---
+++ app/src/main/java/cn/edu/uestc/meet_on_the_road_of_uestc/MainActivity.java
@@ -0,0 +1,13 @@
+package cn.edu.uestc.meet_on_the_road_of_uestc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
