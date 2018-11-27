b1f34183dfa9c707fb16393f9702fdd76bfe673b
ray <romerxzh@foxmail.com>
2018-11-27 21:01:08 +0800

    UPDATE layout for navigation

--- app/src/main/java/cn/edu/uestc/meet_on_the_road_of_uestc/fragment/NavFragment.java
+++ app/src/main/java/cn/edu/uestc/meet_on_the_road_of_uestc/fragment/NavFragment.java
@@ -1,16 +1,20 @@
 package cn.edu.uestc.meet_on_the_road_of_uestc.fragment;
 
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.amap.api.maps.AMap;
 import com.amap.api.maps.CameraUpdateFactory;
@@ -18,26 +22,18 @@ import com.amap.api.maps.MapView;
 import com.amap.api.maps.UiSettings;
 import com.amap.api.maps.model.MyLocationStyle;
 
+import cn.edu.uestc.meet_on_the_road_of_uestc.MyApplication;
 import cn.edu.uestc.meet_on_the_road_of_uestc.R;
-
-import static android.icu.lang.UCharacter.JoiningGroup.E;
 
 public class NavFragment extends Fragment {
     private MapView mMapView;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+//    onClickListener mOnClickListener=new onClickListener();
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view=getLayoutInflater().inflate(R.layout.fragment_nav,container,false);
         //获取地图控件引用
-        onClickListener onClickListener=new onClickListener();
-        ImageView mImageView=view.findViewById(R.id.emergency_help);
-        mImageView.setOnClickListener(onClickListener);
         mMapView = view.findViewById(R.id.map);
         //在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，创建地图
         mMapView.onCreate(savedInstanceState);
@@ -46,28 +42,34 @@ public class NavFragment extends Fragment {
         myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
         myLocationStyle.interval(2000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
         aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
-        aMap.getUiSettings().setMyLocationButtonEnabled(true);//设置默认定位按钮是否显示，非必需设置。
+        aMap.getUiSettings().setMyLocationButtonEnabled(false);//设置默认定位按钮是否显示，非必需设置。
         myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）默认执行此种模式。
         aMap.setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
         UiSettings mUiSettings=aMap.getUiSettings();//获取UI设置类
         aMap.moveCamera(CameraUpdateFactory.zoomTo(19));//设置默认缩放级别
         return view;
     }
-    class onClickListener implements View.OnClickListener{
-        @Override
-        public void onClick(View v) {
-            switch(getId()){
-                case R.id.emergency_help:
-                    Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + "020-61830110"));
-                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+
+        super.onActivityCreated(savedInstanceState);
+
+        Button button = getActivity().findViewById(R.id.emergency_help);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    Intent intent=new Intent(Intent.ACTION_DIAL);
+                    intent.setData(Uri.parse("tel:61830110"));
                     startActivity(intent);
             }
-        }
+        });
     }
+
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        //在activity执行onDestroy时执行mMapView.onDestroy()，销毁地图
+        //在activity执行onDestroy时执行mMapView.onDestroyView()，销毁地图
         mMapView.onDestroy();
     }
     @Override
