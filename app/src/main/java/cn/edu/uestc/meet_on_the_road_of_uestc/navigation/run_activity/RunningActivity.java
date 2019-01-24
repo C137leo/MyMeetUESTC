8fa1a877c2c73de0801da2058ff13568ad639210
ray26 <romerxzh@gmail.com>
2019-01-24 17:55:27 +0800

    Fix Nullpointer of equals

--- app/src/main/java/cn/edu/uestc/meet_on_the_road_of_uestc/navigation/run_activity/RunningActivity.java
+++ app/src/main/java/cn/edu/uestc/meet_on_the_road_of_uestc/navigation/run_activity/RunningActivity.java
@@ -97,14 +97,16 @@ public class RunningActivity extends AppCompatActivity {
     public void setmRunningMapView(){
         MyLocationStyle myLocationStyle;
         myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
-        myLocationStyle.interval(3000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
+        myLocationStyle.interval(1000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
         aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
         aMap.animateCamera(CameraUpdateFactory.zoomTo(19));
         aMap.getUiSettings().setMyLocationButtonEnabled(false);//设置默认Logger配置认定位按钮是否显示，非必需设置。
         aMap.setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
-        if(getIntent().getStringExtra("RunModel").equals("倒计时模式")){
-            Log.d("It is 倒计时模式","倒计时模式");
-            flag_pause=-1;
+        if(getIntent().getStringExtra("RunModel")!=null) {
+            if (getIntent().getStringExtra("RunModel").equals("倒计时模式")) {
+                Log.d("It is 倒计时模式", "倒计时模式");
+                flag_pause = -1;
+            }
         }
         HttpURLConnectionUtils.getNetTime(new HttpURLConnectionUtils.TimeCallBack() {
             @Override
