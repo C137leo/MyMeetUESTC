ac435a0ea8654eeb2491e694ab4f2633af6e4765
C137leo <357822533@qq.com>
2019-02-12 16:30:37 +0800

    Revert "更改了布局文件，从master往下pull解决签名不符的问题"
    This reverts commit a467ed68cbc741633fd5183402e62556b24d77b4.

--- app/src/main/java/cn/edu/uestc/meet_on_the_road_of_uestc/choosepath/adapter/ChoosePathAdapter.java
+++ app/src/main/java/cn/edu/uestc/meet_on_the_road_of_uestc/choosepath/adapter/ChoosePathAdapter.java
@@ -1,55 +1,54 @@
-//package cn.edu.uestc.meet_on_the_road_of_uestc.choosepath.adapter;
-//
-//import android.support.annotation.NonNull;
-//import android.support.v7.widget.RecyclerView;
-//import android.view.LayoutInflater;
-//import android.view.View;
-//import android.view.ViewGroup;
-//import android.widget.ImageView;
-//import android.widget.TextView;
-//
-//import java.util.List;
-//
-//import cn.edu.uestc.meet_on_the_road_of_uestc.choosepath.Path;
-//import cn.edu.uestc.meet_on_the_road_of_uestc.R;
-//
-//public class ChoosePathAdapter extends RecyclerView.Adapter<ChoosePathAdapter.ViewHolder> {
-//    private List<Path> mParhList;
-//    static class ViewHolder extends RecyclerView.ViewHolder{
-//        ImageView pathImage;
-//        TextView pathName;
-//
-//        public ViewHolder(View itemView) {
-//            super(itemView);
-//            pathImage=(ImageView)itemView.findViewById(R.id.path_image);
-//            pathName=(TextView)itemView.findViewById(R.id.path_name);
-//
-//        }
-//    }
-//    public ChoosePathAdapter(List<Path> pathList){
-//        mParhList=pathList;
-//    }
-//    @NonNull
-//    @Override
-//    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-//            View view=LayoutInflater.from(parent.getContext()).inflate(R.layout.path,parent,false);
-//            ViewHolder holder=new ViewHolder(view);
-//
-//        return holder;
-//    }
-//
-//    @Override
-//    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-//            Path path=mParhList.get(position);
-//            holder.pathImage.setImageResource(path.getImageId());
-//            holder.pathName.setText(path.getName());
-//    }
-//
-//
-//    @Override
-//    public int getItemCount() {
-//        return mParhList.size();
-//    }
-//
-//
-//}
+package cn.edu.uestc.meet_on_the_road_of_uestc.choosepath.adapter;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import cn.edu.uestc.meet_on_the_road_of_uestc.choosepath.Path;
+
+public class ChoosePathAdapter extends RecyclerView.Adapter<ChoosePathAdapter.ViewHolder> {
+    private List<Path> mParhList;
+    static class ViewHolder extends RecyclerView.ViewHolder{
+        ImageView pathImage;
+        TextView pathName;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            pathImage=(ImageView)itemView.findViewById(R.id.path_image);
+            pathName=(TextView)itemView.findViewById(R.id.path_name);
+
+        }
+    }
+    public ChoosePathAdapter(List<Path> pathList){
+        mParhList=pathList;
+    }
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view=LayoutInflater.from(parent.getContext()).inflate(R.layout.path,parent,false);
+            ViewHolder holder=new ViewHolder(view);
+
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+            Path path=mParhList.get(position);
+            holder.pathImage.setImageResource(path.getImageId());
+            holder.pathName.setText(path.getName());
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return mParhList.size();
+    }
+
+
+}
