6874e4c6979340576ab7ef3cb9f7fe111b49d08a
C137leo <357822533@qq.com>
2019-02-13 15:34:34 +0800

    基本实现了路径选择功能
    滚动控件用的是scrollview写的，没法实现想要的滑动效果，下面要改成recyclerview。

--- app/src/main/java/cn/edu/uestc/meet_on_the_road_of_uestc/choosepath/activity/ChoosePathActivityPathItem.java
+++ app/src/main/java/cn/edu/uestc/meet_on_the_road_of_uestc/choosepath/activity/ChoosePathActivityPathItem.java
@@ -1,17 +1,41 @@
 package cn.edu.uestc.meet_on_the_road_of_uestc.choosepath.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
-import com.amap.api.maps.MapView;
-import com.amap.api.maps.UiSettings;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import cn.edu.uestc.meet_on_the_road_of_uestc.R;
 
-public class ChoosePathActivityPathItem  extends AppCompatActivity {
+public class ChoosePathActivityPathItem  extends AppCompatActivity  {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-     setContentView(R.layout.activity_choosepath_path);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choosepath_path);
+        ImageView choosePathButton=(ImageView) this.findViewById(R.id.choosethispath1);
+        choosePathButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    Intent intent=new Intent(ChoosePathActivityPathItem.this,ChoosePathActivity.class);
+                    startActivity(intent);
+            }
+        });
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
     }
 
 }
