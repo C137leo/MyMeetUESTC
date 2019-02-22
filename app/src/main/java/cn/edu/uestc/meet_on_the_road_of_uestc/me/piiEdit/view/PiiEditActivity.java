a27c11c90e84ff9413e8a67bca78dc33f5dba487
ray <romerxzh@gmail.com>
2019-02-22 21:13:54 +0800

    修复相机无权限问题

--- app/src/main/java/cn/edu/uestc/meet_on_the_road_of_uestc/me/piiEdit/view/PiiEditActivity.java
+++ app/src/main/java/cn/edu/uestc/meet_on_the_road_of_uestc/me/piiEdit/view/PiiEditActivity.java
@@ -1,6 +1,8 @@
 package cn.edu.uestc.meet_on_the_road_of_uestc.me.piiEdit.view;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
@@ -10,6 +12,8 @@ import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -27,12 +31,13 @@ import java.io.IOException;
 import cn.edu.uestc.meet_on_the_road_of_uestc.MyApplication;
 import cn.edu.uestc.meet_on_the_road_of_uestc.R;
 import cn.edu.uestc.meet_on_the_road_of_uestc.greenDao.GreenDaoHelper;
+import cn.edu.uestc.meet_on_the_road_of_uestc.help.help_manage.entities.AcceptRecycleViewData;
 import cn.edu.uestc.meet_on_the_road_of_uestc.layout.CircleImageView;
 import cn.edu.uestc.meet_on_the_road_of_uestc.me.piiEdit.presenter.PiiEditPrenster;
 import cn.edu.uestc.meet_on_the_road_of_uestc.me.view.PopupWindowSetImage;
 import dev.utils.app.UriUtils;
 
-public class PiiEditActivity extends AppCompatActivity implements View.OnClickListener {
+public class PiiEditActivity extends AppCompatActivity implements View.OnClickListener, ActivityCompat.OnRequestPermissionsResultCallback {
     private static final int ALBUM_REQUEST_CODE = 0;
     private static final int CAMERA_REQUEST_CODE = 1;
     private static final int CROP_PHOTO = 2;
@@ -89,23 +94,19 @@ public class PiiEditActivity extends AppCompatActivity implements View.OnClickLi
                 break;
             }
             case R.id.pop_camera:{
-                //用于保存调用相机拍照后所生成的文件
-                tempFile = new File(Environment.getExternalStorageDirectory().getPath(), GreenDaoHelper.getDaoSession().getStuInfoDao().loadAll().get(0).getStuID() + ".jpg");
-                //跳转到调用系统相机
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                //判断版本
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {   //如果在Android7.0以上,使用FileProvider获取Uri
-                    intent.setFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
-                    contentUri = FileProvider.getUriForFile(PiiEditActivity.this, "cn.edu.uestc.meet_on_the_road_of_uestc", tempFile);
-                    Log.d("contentURI",String.valueOf(contentUri));
-                    intent.putExtra(MediaStore.EXTRA_OUTPUT, contentUri);
-                    intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-                    intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
-                } else {    //否则使用Uri.fromFile(file)方法获取Uri
-                    intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(tempFile));
+                if (Build.VERSION.SDK_INT >= 23) {
+                    int checkCallPhonePermission = ContextCompat.checkSelfPermission(PiiEditActivity.this, Manifest.permission.CAMERA);
+                    if (checkCallPhonePermission != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(PiiEditActivity.this, new String[]{Manifest.permission.CAMERA}, CAMERA_REQUEST_CODE);
+                        return;
+                    } else {
+                        openCamera();
+                        break;
+                    }
+                }else {
+                    openCamera();
+                    break;
                 }
-                startActivityForResult(intent, CAMERA_REQUEST_CODE);
-                break;
             }
             case R.id.pop_pic:{
                 Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
@@ -117,6 +118,55 @@ public class PiiEditActivity extends AppCompatActivity implements View.OnClickLi
         }
     }
 
+    /**
+     * Callback for the result from requesting permissions. This method
+     * is invoked for every call on {@link #requestPermissions(String[], int)}.
+     * <p>
+     * <strong>Note:</strong> It is possible that the permissions request interaction
+     * with the user is interrupted. In this case you will receive empty permissions
+     * and results arrays which should be treated as a cancellation.
+     * </p>
+     *
+     * @param requestCode  The request code passed in {@link #requestPermissions(String[], int)}.
+     * @param permissions  The requested permissions. Never null.
+     * @param grantResults The grant results for the corresponding permissions
+     *                     which is either {@link PackageManager#PERMISSION_GRANTED}
+     *                     or {@link PackageManager#PERMISSION_DENIED}. Never null.
+     * @see #requestPermissions(String[], int)
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case CAMERA_REQUEST_CODE:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    openCamera();
+                } else {
+                    Toast.makeText(PiiEditActivity.this, "相机权限禁用了。请务必开启相机权", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    public void openCamera(){
+        //用于保存调用相机拍照后所生成的文件
+        tempFile = new File(Environment.getExternalStorageDirectory().getPath(), GreenDaoHelper.getDaoSession().getStuInfoDao().loadAll().get(0).getStuID() + ".jpg");
+        //跳转到调用系统相机
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        //判断版本
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {   //如果在Android7.0以上,使用FileProvider获取Uri
+            intent.setFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+            contentUri = FileProvider.getUriForFile(PiiEditActivity.this, "cn.edu.uestc.meet_on_the_road_of_uestc", tempFile);
+            Log.d("contentURI", String.valueOf(contentUri));
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, contentUri);
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+        } else {    //否则使用Uri.fromFile(file)方法获取Uri
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(tempFile));
+        }
+        startActivityForResult(intent, CAMERA_REQUEST_CODE);
+    }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         switch (requestCode){
