275bae17093990fae1d944ec3d44af215cb505aa
ray <romerxzh@gmail.com>
2019-02-25 11:43:35 +0800

    媳妇messageList holders未传入，增加ImageLoafer

--- app/src/main/java/cn/edu/uestc/meet_on_the_road_of_uestc/chat/view/ChatActivity.java
+++ app/src/main/java/cn/edu/uestc/meet_on_the_road_of_uestc/chat/view/ChatActivity.java
@@ -2,18 +2,28 @@ package cn.edu.uestc.meet_on_the_road_of_uestc.chat.view;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.content.FileProvider;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.transition.Transition;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -31,6 +41,7 @@ import cn.edu.uestc.meet_on_the_road_of_uestc.me.piiEdit.view.PiiEditActivity;
 import cn.jiguang.imui.chatinput.ChatInputView;
 import cn.jiguang.imui.chatinput.listener.OnMenuClickListener;
 import cn.jiguang.imui.chatinput.model.FileItem;
+import cn.jiguang.imui.commons.ImageLoader;
 import cn.jiguang.imui.commons.models.IMessage;
 import cn.jiguang.imui.messages.MessageList;
 import cn.jiguang.imui.messages.MsgListAdapter;
@@ -48,12 +59,21 @@ public class ChatActivity extends ChatBaseActivity implements View.OnClickListen
     int IMAGE_PICK_REQUEST_CODE=0;
     int IMAGE_SHOOT_REQUEST_CODE=1;
     MessageList messageList;
-    MsgListAdapter adapter = new MsgListAdapter<>(userID, null, null);
+    MsgListAdapter.HoldersConfig holdersConfig=new MsgListAdapter.HoldersConfig();
+    MsgListAdapter adapter;
     Uri contentUri;
     File tempFile;
+    final float density = getResources().getDisplayMetrics().density;
+    final float MIN_WIDTH = 60 * density;
+    final float MAX_WIDTH = 200 * density;
+    final float MIN_HEIGHT = 60 * density;
+    final float MAX_HEIGHT = 200 * density;
+    ImageLoader imageLoader;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
+        initImageLoager();
+        adapter = new MsgListAdapter<>(userID, holdersConfig, imageLoader);
         chatPresnter.attchView(iView);
         userID=getIntent().getStringExtra("StuID");
         chatPresnter.startChat(userID);
@@ -239,5 +259,110 @@ public class ChatActivity extends ChatBaseActivity implements View.OnClickListen
                 receiveVideoMessage.setUserInfo(reciverUser);
                 adapter.addToStart(receiveVideoMessage,true);
         }
+    }
+
+    public void initImageLoager(){
+        imageLoader = new ImageLoader() {
+            @Override
+            public void loadAvatarImage(ImageView avatarImageView, String string) {
+                // You can use other image load libraries.
+                if (string.contains("R.drawable")) {
+                    Integer resId = getResources().getIdentifier(string.replace("R.drawable.", ""),
+                            "drawable", getPackageName());
+
+                    avatarImageView.setImageResource(resId);
+                } else {
+                    Glide.with(ChatActivity.this)
+                            .load(string)
+                            .apply(new RequestOptions().placeholder(R.drawable.aurora_headicon_default))
+                            .into(avatarImageView);
+                }
+            }
+
+            /**
+             * Load image message
+             * @param imageView Image message's ImageView.
+             * @param string A file path, or a uri or url.
+             */
+            @Override
+            public void loadImage(final ImageView imageView, String string) {
+                // You can use other image load libraries.
+                Glide.with(getApplicationContext())
+                        .asBitmap()
+                        .load(string)
+                        .apply(new RequestOptions().fitCenter().placeholder(R.drawable.aurora_picture_not_found))
+                        .into(new SimpleTarget<Bitmap>() {
+                            @Override
+                            public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+                                int imageWidth = resource.getWidth();
+                                int imageHeight = resource.getHeight();
+                                Log.d("ImageSize", "Image width " + imageWidth + " height: " + imageHeight);
+
+                                // 裁剪 bitmap
+                                float width, height;
+                                if (imageWidth > imageHeight) {
+                                    if (imageWidth > MAX_WIDTH) {
+                                        float temp = MAX_WIDTH / imageWidth * imageHeight;
+                                        height = temp > MIN_HEIGHT ? temp : MIN_HEIGHT;
+                                        width = MAX_WIDTH;
+                                    } else if (imageWidth < MIN_WIDTH) {
+                                        float temp = MIN_WIDTH / imageWidth * imageHeight;
+                                        height = temp < MAX_HEIGHT ? temp : MAX_HEIGHT;
+                                        width = MIN_WIDTH;
+                                    } else {
+                                        float ratio = imageWidth / imageHeight;
+                                        if (ratio > 3) {
+                                            ratio = 3;
+                                        }
+                                        height = imageHeight * ratio;
+                                        width = imageWidth;
+                                    }
+                                } else {
+                                    if (imageHeight > MAX_HEIGHT) {
+                                        float temp = MAX_HEIGHT / imageHeight * imageWidth;
+                                        width = temp > MIN_WIDTH ? temp : MIN_WIDTH;
+                                        height = MAX_HEIGHT;
+                                    } else if (imageHeight < MIN_HEIGHT) {
+                                        float temp = MIN_HEIGHT / imageHeight * imageWidth;
+                                        width = temp < MAX_WIDTH ? temp : MAX_WIDTH;
+                                        height = MIN_HEIGHT;
+                                    } else {
+                                        float ratio = imageHeight / imageWidth;
+                                        if (ratio > 3) {
+                                            ratio = 3;
+                                        }
+                                        width = imageWidth * ratio;
+                                        height = imageHeight;
+                                    }
+                                }
+                                ViewGroup.LayoutParams params = imageView.getLayoutParams();
+                                params.width = (int) width;
+                                params.height = (int) height;
+                                imageView.setLayoutParams(params);
+                                Matrix matrix = new Matrix();
+                                float scaleWidth = width / imageWidth;
+                                float scaleHeight = height / imageHeight;
+                                matrix.postScale(scaleWidth, scaleHeight);
+                                imageView.setImageBitmap(Bitmap.createBitmap(resource, 0, 0, imageWidth, imageHeight, matrix, true));
+                            }
+                        });
+            }
+
+            /**
+             * Load video message
+             * @param imageCover Video message's image cover
+             * @param uri Local path or url.
+             */
+            @Override
+            public void loadVideo(ImageView imageCover, String uri) {
+                long interval = 5000 * 1000;
+                Glide.with(ChatActivity.this)
+                        .asBitmap()
+                        .load(uri)
+                        // Resize image view by change override size.
+                        .apply(new RequestOptions().frame(interval).override(200, 400))
+                        .into(imageCover);
+            }
+        };
     }
 }
