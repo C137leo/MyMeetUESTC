908a84a616f36351f75cf872b838a58b07fd668a
ray <romerxzh@gmail.com>
2019-03-05 20:26:42 +0800

    Update to gradle 3.3.2

---
+++ app/src/main/java/cn/edu/uestc/meet_on_the_road_of_uestc/layout/AppointmentMeTitle.java
@@ -0,0 +1,22 @@
+package cn.edu.uestc.meet_on_the_road_of_uestc.layout;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+public class AppointmentTypeSelectedLayout extends RelativeLayout {
+
+    public AppointmentTypeSelectedLayout(Context context) {
+        super(context);
+        initView(context);
+    }
+
+    public AppointmentTypeSelectedLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initView(context);
+    }
+
+    public void initView(Context context){
+
+    }
+}
