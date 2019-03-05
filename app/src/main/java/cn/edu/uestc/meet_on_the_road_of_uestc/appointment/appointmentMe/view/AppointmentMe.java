5a9a54d82e7665bf1cdb084c894741b7f4efc6da
ray <romerxzh@gmail.com>
2019-03-05 20:57:11 +0800

    初始化Appointment选择标题

--- app/src/main/java/cn/edu/uestc/meet_on_the_road_of_uestc/appointment/appointmentMe/view/AppointmentMe.java
+++ app/src/main/java/cn/edu/uestc/meet_on_the_road_of_uestc/appointment/appointmentMe/view/AppointmentMe.java
@@ -6,15 +6,18 @@ import android.support.v7.app.AppCompatActivity;
 
 import cn.edu.uestc.meet_on_the_road_of_uestc.R;
 import cn.edu.uestc.meet_on_the_road_of_uestc.appointment.appointmentMe.prenster.AppointmentPrensterMe;
+import cn.edu.uestc.meet_on_the_road_of_uestc.layout.AppointmentMeTitle;
 
 public class AppointmentMe extends AppCompatActivity {
     AppointmentPrensterMe appointmentPrensterMe;
+    AppointmentMeTitle appointmentMeTitle;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         appointmentPrensterMe=new AppointmentPrensterMe(AppointmentMe.this);
         appointmentPrensterMe.attchView(iVew);
         setContentView(R.layout.activity_appointment_me);
+        appointmentMeTitle=findViewById(R.id.appointment_me_title);
     }
 
     IVew iVew=new IVew() {
--- app/src/main/java/cn/edu/uestc/meet_on_the_road_of_uestc/layout/AppointmentMeTitle.java
+++ app/src/main/java/cn/edu/uestc/meet_on_the_road_of_uestc/layout/AppointmentMeTitle.java
@@ -2,21 +2,55 @@ package cn.edu.uestc.meet_on_the_road_of_uestc.layout;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-public class AppointmentTypeSelectedLayout extends RelativeLayout {
+import cn.edu.uestc.meet_on_the_road_of_uestc.R;
 
-    public AppointmentTypeSelectedLayout(Context context) {
+public class AppointmentMeTitle extends RelativeLayout implements View.OnClickListener {
+    TextView appointmentAccept;
+    TextView appointmentPublish;
+    AppointmentMeTitleListener appointmentMeTitleListener;
+    public AppointmentMeTitle(Context context) {
         super(context);
         initView(context);
     }
 
-    public AppointmentTypeSelectedLayout(Context context, AttributeSet attrs) {
+    public AppointmentMeTitle(Context context, AttributeSet attrs) {
         super(context, attrs);
         initView(context);
     }
 
     public void initView(Context context){
+        LayoutInflater.from(context).inflate(R.layout.appointment_me_title,this,true);
+        appointmentAccept=findViewById(R.id.appointment_accept);
+        appointmentPublish=findViewById(R.id.appointment_publish);
+        appointmentAccept.setOnClickListener(this);
+        appointmentPublish.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.appointment_accept:
+                appointmentMeTitleListener.appointmentAcceptClick();
+                appointmentAccept.setBackground(getResources().getDrawable(R.drawable.finish_help_button));
+                appointmentPublish.setBackground(null);
+            case R.id.appointment_publish:
+                appointmentMeTitleListener.appointmentPublishClick();
+                appointmentAccept.setBackground(null);
+                appointmentPublish.setBackground(getResources().getDrawable(R.drawable.finish_help_button));
+        }
+    }
+
+    public interface AppointmentMeTitleListener{
+        void appointmentAcceptClick();
+        void appointmentPublishClick();
+    }
+
+    public void initAppointmentMeTitleClick(AppointmentMeTitleListener appointmentMeTitleListener){
+        this.appointmentMeTitleListener=appointmentMeTitleListener;
     }
 }
